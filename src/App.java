c2902265f0fd2d8de071e7eceb4c53bad575e6a1
Katarzyna Drobniewicz <kasia.drobniewicz@gmail.com>
2018-05-28 11:45:01 +0200

    Add Application file

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        NearbyElements nearbyElements = new NearbyElements();
+
+        nearbyElements.printNearbyElements(nearbyElements.returnNearbyElements(0, 2, 2));
+
+    }
+}
